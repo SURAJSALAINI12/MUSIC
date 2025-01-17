7ce169ad7e452be91840b9a1ade13152acaba576
SURAJSALAINI12 <surajrajsaini218@gmail.com>
2025-01-17 09:44:29 +0530

    Update start.py

--- BADMUSIC/utils/inline/start.py
+++ BADMUSIC/utils/inline/start.py
@@ -8,16 +8,16 @@ def start_pannel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text="💠 𝐀𝙳𝙳 𝙼𝙴 𝙸𝙽 𝙽𝙴𝚆 𝙶𝚁𝙾𝚄𝙿𝚂 💠",
+                text="♥️⃟🌿𝐀𝙳𝙳 𝙼𝙴 𝙸𝙽 𝙽𝙴𝚆 𝙶𝚁𝙾𝚄𝙿𝚂♥️⃟🌿",
                 url=f"https://t.me/{app.username}?startgroup=true",
             ),
         ],
         [
-            InlineKeyboardButton(text="۞ 𝐇𝙴𝙻𝙿 ۞", callback_data="settings_back_helper"),
-            InlineKeyboardButton(text="☢ 𝐒𝙴𝚃 ☢", callback_data="settings_helper"),
+            InlineKeyboardButton(text="♥️⃟🌿𝐇𝙴𝙻𝙿♥️⃟🌿", callback_data="settings_back_helper"),
+            InlineKeyboardButton(text="♥️⃟🌿𝐒𝙴𝚃♥️⃟🌿", callback_data="settings_helper"),
         ],
         [
-            InlineKeyboardButton(text="✡ 𝐆𝚁𝙾𝚄𝙿 ✡", url=config.SUPPORT_GROUP),
+            InlineKeyboardButton(text="♥️⃟🌿𝐆𝚁𝙾𝚄𝙿♥️⃟🌿", url=config.SUPPORT_GROUP),
         ],
     ]
     return buttons
@@ -27,17 +27,17 @@ def private_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text="💠 𝐀𝙳𝙳 𝙼𝙴 𝙸𝙽 𝙽𝙴𝚆 𝙶𝚁𝙾𝚄𝙿𝚂 💠",
+                text="♥️⃟🌿𝐀𝙳𝙳 𝙼𝙴 𝙸𝙽 𝙽𝙴𝚆 𝙶𝚁𝙾𝚄𝙿𝚂♥️⃟🌿",
                 url=f"https://t.me/{app.username}?startgroup=true",
             )
         ],
         [
-            InlineKeyboardButton(text="𝐆𝚁𝙾𝚄𝙿✨", url=config.SUPPORT_GROUP),
-            InlineKeyboardButton(text="𝐌ᴏʀᴇ🥀", url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text="♥️⃟🌿𝐆𝚁𝙾𝚄𝙿♥️⃟🌿", url=config.SUPPORT_GROUP),
+            InlineKeyboardButton(text="♥️⃟🌿𝐌ᴏʀᴇ♥️⃟🌿", url=config.SUPPORT_CHANNEL),
         ],
         [
             InlineKeyboardButton(
-                text="۞ 𝐅𝙴𝙰𝚃𝚄𝚁𝙴𝚂 ۞", callback_data="settings_back_helper"
+                text="♥️⃟🌿 𝐅𝙴𝙰𝚃𝚄𝚁𝙴𝚂 ♥️⃟🌿", callback_data="settings_back_helper"
             )
         ],
     ]
@@ -48,18 +48,18 @@ def alive_panel(_):
     buttons = [
             [
             InlineKeyboardButton(
-                text="📩 ꜱᴛᴀʀᴛ ᴘᴍ",
+                text="♥️⃟🌿 ꜱᴛᴀʀᴛ ᴘᴍ ♥️⃟🌿",
                 url=f"https://t.me/{app.username}",
             ),
         ],
         [
             InlineKeyboardButton(
-                text="✿︎ ᴀᴅᴅ ᴍᴇ ✿︎", url=f"https://t.me/{app.username}?startgroup=true"
+                text="♥️⃟🌿 ᴀᴅᴅ ᴍᴇ ♥️⃟🌿", url=f"https://t.me/{app.username}?startgroup=true"
             ),
             InlineKeyboardButton(text=_["S_B_3"], url=f"{SUPPORT_GROUP}"),
         ],
                 [
-            InlineKeyboardButton(text="🎧 ꜱᴘᴏᴛɪꜰʏ", web_app=WebAppInfo(url="https://open.spotify.com/"),)
+            InlineKeyboardButton(text="♥️⃟🌿 ꜱᴘᴏᴛɪꜰʏ ♥️⃟🌿", web_app=WebAppInfo(url="https://open.spotify.com/"),)
             
         ],
     ]
@@ -71,19 +71,19 @@ def music_start_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text="▪️ᴀᴅᴅ ᴍᴇ ɪɴ ɴᴇᴡ ɢʀᴏᴜᴘ▪️",
+                text="♥️⃟🌿ᴀᴅᴅ ᴍᴇ ɪɴ ɴᴇᴡ ɢʀᴏᴜᴘ♥️⃟🌿",
                 url=f"https://t.me/{app.username}?startgroup=true",
             )
         ],
         [
-            InlineKeyboardButton(text="▪️ᴀʙᴏᴜᴛ▪️", callback_data="about"),
-            InlineKeyboardButton(text="▪️sᴜᴘᴘᴏʀᴛ▪️", callback_data="support"),
+            InlineKeyboardButton(text="♥️⃟🌿ᴀʙᴏᴜᴛ♥️⃟🌿", callback_data="about"),
+            InlineKeyboardButton(text="♥️⃟🌿sᴜᴘᴘᴏʀᴛ♥️⃟🌿", callback_data="support"),
         ],
         [
-            InlineKeyboardButton(text="▪️ꜱᴘᴏᴛɪꜰʏ▪️", web_app=WebAppInfo(url="https://open.spotify.com/"),)
+            InlineKeyboardButton(text="♥️⃟🌿ꜱᴘᴏᴛɪꜰʏ♥️⃟🌿", web_app=WebAppInfo(url="https://open.spotify.com/"),)
             
         ],
-        [InlineKeyboardButton(text="▪️ ғᴇᴀᴛᴜʀᴇ ▪️", callback_data="feature")],
+        [InlineKeyboardButton(text="♥️⃟🌿ғᴇᴀᴛᴜʀᴇ♥️⃟🌿", callback_data="feature")],
         
     ]
     return buttons
